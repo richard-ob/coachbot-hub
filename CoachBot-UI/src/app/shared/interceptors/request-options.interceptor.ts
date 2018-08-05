e3fa8ed664df9811c83ab3dc278c3e52368bd016
Richard O'Brien <richard.obrien4@gmail.com>
2018-08-05 15:24:59 +0100

    Linted

--- CoachBot-UI/src/app/shared/interceptors/request-options.interceptor.ts
+++ CoachBot-UI/src/app/shared/interceptors/request-options.interceptor.ts
@@ -1,6 +1,6 @@
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Injectable } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RequestOptionsInterceptor implements HttpInterceptor {

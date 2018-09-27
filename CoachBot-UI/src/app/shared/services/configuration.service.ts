a00d759ca6c399248b71adefe8e2fd47879a4129
Richard O'Brien <richard.obrien4@gmail.com>
2018-09-27 22:31:44 +0100

    Lint file

--- CoachBot-UI/src/app/shared/services/configuration.service.ts
+++ CoachBot-UI/src/app/shared/services/configuration.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Configuration } from '../../model/configuration';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 

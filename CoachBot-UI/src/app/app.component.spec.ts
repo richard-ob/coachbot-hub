59817ab4913670462f37df792aa73ca1079b2a2b
Richard O'Brien <richard.obrien4@gmail.com>
2018-09-30 22:12:43 +0100

    Add initial basic unit test

--- CoachBot-UI/src/app/app.component.spec.ts
+++ CoachBot-UI/src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +20,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render content container', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CoachBot-UI!');
+    expect(compiled.querySelector('.container-fluid')).toBeTruthy();
   }));
 });

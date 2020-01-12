16603e46d255ad70f5dd8a7827896a0ac1320d7b
Richard O'Brien <richard.obrien4@gmail.com>
2020-01-12 22:39:59 +0000

    Fix issue with precondition not working

--- CoachBot/Bot/Preconditions/ChannelConfiguredPrecondition.cs
+++ CoachBot/Bot/Preconditions/ChannelConfiguredPrecondition.cs
@@ -11,14 +11,17 @@ namespace CoachBot.Preconditions
     {
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider map)
         {
-            var channelService = map.GetService<ChannelService>();
-            var configService = map.GetService<ConfigService>();
+            using (var scope = map.CreateScope())
+            {
+                var channelService = scope.ServiceProvider.GetService<ChannelService>();
+                var configService = scope.ServiceProvider.GetService<ConfigService>();
 
-            if (channelService.GetChannelByDiscordId(context.Channel.Id) is null)
-                return Task.FromResult(PreconditionResult.FromError($":wrench: This channel is not yet configured. To configure the channel, please visit {configService.Config.ClientUrl}"));
+                if (channelService.GetChannelByDiscordId(context.Channel.Id) is null)
+                    return Task.FromResult(PreconditionResult.FromError($":wrench: This channel is not yet configured. To configure the channel, please visit {configService.Config.ClientUrl}"));
 
-            if (channelService.GetChannelByDiscordId(context.Channel.Id).Region is null)
-                return Task.FromResult(PreconditionResult.FromError($":earth_asia: This channel does not have a region set. To configure the channel, please visit {configService.Config.ClientUrl}"));
+                if (channelService.GetChannelByDiscordId(context.Channel.Id).Region is null)
+                    return Task.FromResult(PreconditionResult.FromError($":earth_asia: This channel does not have a region set. To configure the channel, please visit {configService.Config.ClientUrl}"));
+            }
 
             return Task.FromResult(PreconditionResult.FromSuccess());
         }

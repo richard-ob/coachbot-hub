34b16b20865ebc6d8542985f4cc591dfac7f0dfb
Richard O'Brien <richard.obrien4@gmail.com>
2020-04-01 22:25:56 +0100

    Add skeleton Tournament Controller

---
+++ CoachBot/Controllers/TournamentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoachBot.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/player-team")]
+    [ApiController]
+    public class TournamentController : Controller
+    {
+
+        public TournamentController()
+        {
+
+        }
+    }
+}

893ff43d4ca513470762b563d2a9ea34f64c3f17
Richard O'Brien <richard.obrien4@gmail.com>
2020-05-31 17:54:26 +0100

    Fix Discord integration issues

--- CoachBot.Hub.UI/coachbot-ui/src/app/pages/hub/team-editor/team-editor-discord-integration/discord-guild-editor/discord-guild-editor.component.ts
+++ CoachBot.Hub.UI/coachbot-ui/src/app/pages/hub/team-editor/team-editor-discord-integration/discord-guild-editor/discord-guild-editor.component.ts
@@ -4,6 +4,7 @@ import { TeamService } from '../../../shared/services/team.service';
 import { DiscordGuild } from '../../../shared/model/discord-guild.model';
 import { GuildService } from '../../../shared/services/guild.service';
 import { Team } from '../../../shared/model/team.model';
+import { PlayerService } from '@pages/hub/shared/services/player.service';
 
 @Component({
     selector: 'app-discord-guild-editor',
@@ -16,13 +17,19 @@ export class DiscordGuildEditorComponent {
     teamId: number;
     team: Team;
     selectedDiscordGuildId: string;
+    userDiscordAccountLinked: boolean;
     discordGuildPreviouslySet = false;
     isEditing = false;
     isLoading = true;
 
-    constructor(private discordService: DiscordService, private teamService: TeamService, private guildService: GuildService) { }
+    constructor(
+        private discordService: DiscordService,
+        private teamService: TeamService,
+        private guildService: GuildService,
+        private playerService: PlayerService
+    ) { }
 
-    openGuildEditor(teamId: number, discordGuildId: string) {
+    openGuildEditor(teamId: number, discordGuildId: string = null) {
         this.isLoading = true;
         this.isEditing = true;
         this.teamId = teamId;
@@ -32,7 +39,10 @@ export class DiscordGuildEditorComponent {
             this.discordGuilds = discordGuilds;
             this.teamService.getTeam(teamId).subscribe(team => {
                 this.team = team;
-                this.isLoading = false;
+                this.playerService.getCurrentPlayer().subscribe(player => {
+                    this.userDiscordAccountLinked = player.discordUserId !== null;
+                    this.isLoading = false;
+                });
             });
         });
     }
--- CoachBot.Hub.UI/coachbot-ui/src/app/pages/hub/team-editor/team-editor-discord-integration/team-editor-discord-integration.component.ts
+++ CoachBot.Hub.UI/coachbot-ui/src/app/pages/hub/team-editor/team-editor-discord-integration/team-editor-discord-integration.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { TeamService } from '../../shared/services/team.service';
 import { Channel } from '../../shared/model/channel.model';
 import { ChannelService } from '../../shared/services/channel.service';
 import { Team } from '../../shared/model/team.model';
 import { ActivatedRoute } from '@angular/router';
+import { DiscordGuildEditorComponent } from './discord-guild-editor/discord-guild-editor.component';
 
 @Component({
     selector: 'app-team-editor-discord-integration',
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TeamEditorDiscordIntegrationComponent implements OnInit {
 
+    @ViewChild(DiscordGuildEditorComponent, { static: false }) guildEditor: DiscordGuildEditorComponent;
     @Input() teamId: number;
     team: Team;
     channels: Channel[];
@@ -29,6 +31,11 @@ export class TeamEditorDiscordIntegrationComponent implements OnInit {
                 if (this.team.guild && this.team.guild.discordGuildId) {
                     this.isDiscordGuildSet = true;
                     this.loadChannels();
+                } else {
+                    this.isDiscordGuildSet = false;
+                    this.isLoading = false;
+                    this.isGuildEditorOpen = true;
+                    this.guildEditor.openGuildEditor(this.teamId);
                 }
             });
         });

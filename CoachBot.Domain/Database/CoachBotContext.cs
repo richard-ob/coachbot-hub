6439a5bbaa27c51f5622a7a505844ac4fb7f18a7
Richard O'Brien <richard.obrien4@gmail.com>
2020-07-26 21:06:40 +0100

    Ensure uniqueness of SteamID & DiscordID

--- CoachBot.Domain/Database/CoachBotContext.cs
+++ CoachBot.Domain/Database/CoachBotContext.cs
@@ -151,6 +151,8 @@ namespace CoachBot.Database
             modelBuilder.Entity<Server>().HasIndex(s => new { s.Address }).IsUnique(true);
             modelBuilder.Entity<Region>().HasIndex(r => new { r.RegionName }).IsUnique(true);
             modelBuilder.Entity<Team>().HasIndex(t => new { t.TeamCode, t.RegionId }).IsUnique(true);
+            modelBuilder.Entity<Player>().HasIndex(p => new { p.SteamID }).IsUnique(true);
+            modelBuilder.Entity<Player>().HasIndex(p => new { p.DiscordUserId }).IsUnique(true);
             modelBuilder.Entity<Position>().HasIndex(p => new { p.Name }).IsUnique(true);
             modelBuilder.Entity<PlayerLineupPosition>().HasIndex(ptp => new { ptp.PositionId, ptp.LineupId }).IsUnique(true);
             modelBuilder.Entity<TournamentStaff>().HasIndex(tes => new { tes.PlayerId, tes.TournamentId }).IsUnique(true);

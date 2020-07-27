f9986311c2e51b7eae06cb67179af8fcf6bd92b7
Richard O'Brien <richard.obrien4@gmail.com>
2020-07-27 21:26:17 +0100

    Remove player adding on startup due to conflict with importer

--- CoachBot.Domain/Database/DbInitializer.cs
+++ CoachBot.Domain/Database/DbInitializer.cs
@@ -15,15 +15,6 @@ namespace CoachBot.Database
                 // context.Lineups.RemoveRange(context.Lineups.Where(t => !context.Matchups.Any(m => m.LineupHomeId == t.Id || m.LineupAwayId == t.Id))); // Remove orphaned teams
                 //context.PlayerTeamPositions.RemoveRange(context.PlayerTeamPositions.Where(ptp => ptp.CreatedDate < DateTime.Now.AddDays(-1)).Where(ptp => ptp.Team == null || ptp.Team.Match == null || ptp.Team.Match.ReadiedDate == null)); // Clear any signings older than a day
 
-                var player = new Player()
-                {
-                    Name = "Thing'e'",
-                    SteamID = 76561197960374238,
-                    DiscordUserId = 166153339610857472,
-                    HubRole = PlayerHubRole.Owner,
-                    Rating = 7.2
-                };
-                context.Players.Add(player);
                 context.SaveChanges();
             }
             catch

0273c9b78b177124e4667ad0e5fb79695a1ba1f4
Richard O'Brien <richard.obrien4@gmail.com>
2020-07-29 16:44:41 +0100

    Add new aggregate view of player stats, with optional position view (as position view looks a bit messy for some matches)

--- CoachBot.UI/src/app/pages/hub/match-overview/components/match-overview-player-statistics/match-overview-player-statistics.component.ts
+++ CoachBot.UI/src/app/pages/hub/match-overview/components/match-overview-player-statistics/match-overview-player-statistics.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PlayerService } from '@pages/hub/shared/services/player.service';
 import { PlayerStatisticFilters } from '@pages/hub/shared/model/dtos/paged-player-statistics-request-dto.model';
-import { PlayerPositionMatchStatistics } from '@pages/hub/shared/model/player-position-match-statistics.model';
 import SortingUtils from '@shared/utilities/sorting-utilities';
 import { Router } from '@angular/router';
+import { PlayerMatchStatistics } from '@pages/hub/shared/model/player-match-statistics.model';
+import { PlayerPositionMatchStatistics } from '@pages/hub/shared/model/player-position-match-statistics.model';
 
 @Component({
     selector: 'app-match-overview-player-statistics',
@@ -13,11 +14,12 @@ export class MatchOverviewPlayerStatisticsComponent implements OnInit {
 
     @Input() matchId: number;
     filters = new PlayerStatisticFilters();
-    players: PlayerPositionMatchStatistics[];
+    players: PlayerMatchStatistics[] | PlayerPositionMatchStatistics[];
     currentPage = 1;
     sortBy: string = null;
     sortOrder = 'ASC';
     showMoreStats = false;
+    showAggregateStats = true;
     isLoading = true;
     isSorting = false;
 
@@ -32,12 +34,43 @@ export class MatchOverviewPlayerStatisticsComponent implements OnInit {
         this.isSorting = true;
         this.sortOrder = SortingUtils.getSortOrder(this.sortBy, sortBy, this.sortOrder);
         this.sortBy = sortBy;
+        if (this.showAggregateStats) { // Aggregated for whole match (no positions)
+            this.loadAggregateStats();
+        } else { // By Position
+            this.loadPositionStats();
+        }
+    }
+
+    loadAggregateStats() {
+        if (this.sortBy === 'Position.Name') {
+            this.sortBy = null;
+            this.sortOrder = null;
+        }
+        this.playerService.getPlayerMatchStatistics(1, 50, this.sortBy, this.sortOrder, this.filters).subscribe(response => {
+            this.players = response.items;
+            this.currentPage = response.page;
+            this.isLoading = false;
+            this.isSorting = false;
+        });
+    }
+
+    loadPositionStats() {
         this.playerService.getPlayerPositionMatchStatistics(1, 50, this.sortBy, this.sortOrder, this.filters).subscribe(response => {
             this.players = response.items;
             this.currentPage = response.page;
             this.isLoading = false;
             this.isSorting = false;
         });
+    }
+
+    toggleStatAggregation() {
+        this.isLoading = true;
+        this.showAggregateStats = !this.showAggregateStats;
+        if (this.showAggregateStats) {
+            this.loadAggregateStats();
+        } else {
+            this.loadPositionStats();
+        }
     }
 
     navigateToPlayerProfile(playerId: number) {

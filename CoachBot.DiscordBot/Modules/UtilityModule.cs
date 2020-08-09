160987ce603a91c1b8af457d85d2445015044e61
Richard O'Brien <richard.obrien4@gmail.com>
2020-08-09 11:27:27 +0100

    Add !help command

---
+++ CoachBot.DiscordBot/Modules/UtilityModule.cs
@@ -0,0 +1,24 @@
+﻿using CoachBot.Domain.Model;
+using CoachBot.Shared.Model;
+using CoachBot.Tools;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace CoachBot.Modules
+{
+    public class UtilityModule : ModuleBase
+    {
+        private readonly Config _config;
+
+        public UtilityModule(Config config)
+        {
+            _config = config;
+        }
+
+        [Command("!help")]
+        public async Task HelpAsync()
+        {
+            await ReplyAsync("", embed: DiscordEmbedHelper.GenerateEmbed($"A full list of Coach commands is available at https://{_config.WebServerConfig.ClientUrl}bot-manual.", ServiceResponseStatus.Info));
+        }
+    }
+}

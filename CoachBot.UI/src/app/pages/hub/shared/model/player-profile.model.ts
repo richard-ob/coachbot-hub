2c7cb37300ec6401f43c794cca5238c65baa52a7
Richard O'Brien <richard.obrien4@gmail.com>
2020-08-12 17:27:04 +0100

    Add club team role to PlayerProfile

--- CoachBot.UI/src/app/pages/hub/shared/model/player-profile.model.ts
+++ CoachBot.UI/src/app/pages/hub/shared/model/player-profile.model.ts
@@ -1,11 +1,13 @@
 import { Country } from './country.model';
 import { Team } from './team.model';
 import { Position } from './position';
+import { TeamRole } from './team-role.enum';
 
 export interface PlayerProfile {
     name: string;
     country: Country;
     clubTeam: Team;
+    clubTeamRole: TeamRole;
     nationalTeam: Team;
     position: Position;
 }

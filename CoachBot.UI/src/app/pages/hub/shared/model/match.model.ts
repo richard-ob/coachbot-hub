ba80c3d0ebe0124a29664a298803b3ce7d204b82
Richard O'Brien <richard.obrien4@gmail.com>
2020-08-15 18:31:08 +0100

    Add missing properties from match model

--- CoachBot.UI/src/app/pages/hub/shared/model/match.model.ts
+++ CoachBot.UI/src/app/pages/hub/shared/model/match.model.ts
@@ -1,5 +1,6 @@
 import { Server } from './server.model';
 import { MatchStatistics } from './match-statistics.model';
+import { Tournament } from './tournament.model';
 
 export interface Match {
     id: number;
@@ -11,6 +12,8 @@ export interface Match {
     server: Server;
     matchStatisticsId?: number;
     matchStatistics: MatchStatistics;
+    tournamentId?: number;
+    tournament?: Tournament;
     kickOff: Date;
     createdDate: Date;
 }

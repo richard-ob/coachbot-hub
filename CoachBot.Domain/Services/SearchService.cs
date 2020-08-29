de0ed572eef4006a56606e07d283e4dba49ae3c2
Richard O'Brien <richard.obrien4@gmail.com>
2020-08-29 22:03:51 +0100

    Use TimeSpan.FromMinutes

--- CoachBot.Domain/Services/SearchService.cs
+++ CoachBot.Domain/Services/SearchService.cs
@@ -80,7 +80,7 @@ namespace CoachBot.Domain.Services
 
         public async void TimeoutSearch(int channelId)
         {
-            await Task.Delay(30 * 60 * 1000);
+            await Task.Delay(TimeSpan.FromMinutes(30));
             if (_coachBotContext.Searches.Any(s => s.ChannelId == channelId))
             {
                 var channel = _coachBotContext.Channels.Find(channelId);

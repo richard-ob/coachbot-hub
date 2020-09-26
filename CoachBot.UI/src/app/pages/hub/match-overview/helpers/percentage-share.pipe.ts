463f815d0286ee576093b1b0a3c77109572f02c7
Richard O'Brien <richard.obrien4@gmail.com>
2020-09-26 13:00:16 +0100

    Handle 0 values for circle graphs

--- CoachBot.UI/src/app/pages/hub/match-overview/helpers/percentage-share.pipe.ts
+++ CoachBot.UI/src/app/pages/hub/match-overview/helpers/percentage-share.pipe.ts
@@ -5,6 +5,10 @@ export class PercentageSharePipe implements PipeTransform {
     transform(value: number, otherValues: number[], precision: number = 0): string {
         const total = value + otherValues.reduce((a, b) => a + b, 0);
 
+        if (!total) {
+            return '0';
+        }
+
         return ((value / total) * 100).toFixed(precision);
     }
 }

3318e5d6f59cf8738e573e29d668cd1992cf43e7
Richard O'Brien <richard.obrien4+git@gmail.com>
2020-12-16 18:43:08 +0000

    Add security check to Tournament Match List

--- CoachBot.UI/src/app/pages/hub/tournaments/tournament-match-list/tournament-match-list.component.ts
+++ CoachBot.UI/src/app/pages/hub/tournaments/tournament-match-list/tournament-match-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TournamentService } from '@pages/hub/shared/services/tournament.service';
 import { Tournament } from '@pages/hub/shared/model/tournament.model';
+import { PlayerService } from '@pages/hub/shared/services/player.service';
+import { Player } from '@pages/hub/shared/model/player.model';
+import { TournamentStaffRole } from '@pages/hub/shared/model/tournament-staff-role.model';
 
 @Component({
     selector: 'app-tournament-match-list',
@@ -11,18 +14,20 @@ export class TournamentMatchListComponent implements OnInit {
 
     tournamentId: number;
     tournament: Tournament;
+    currentPlayer: Player;
+    accessDenied: boolean;
     isLoading = true;
 
     constructor(
         private tournamentService: TournamentService,
         private route: ActivatedRoute,
+        private playerService: PlayerService,
         private router: Router
     ) { }
 
     ngOnInit() {
         this.route.paramMap.pipe().subscribe(params => {
             this.tournamentId = +params.get('id');
-            console.log(this.tournamentId);
             this.loadTournament();
         });
     }
@@ -30,8 +35,14 @@ export class TournamentMatchListComponent implements OnInit {
     loadTournament() {
         this.isLoading = true;
         this.tournamentService.getTournament(this.tournamentId).subscribe(tournament => {
-            this.tournament = tournament;
-            this.isLoading = false;
+            this.playerService.getCurrentPlayer().subscribe((currentPlayer) => {
+                if (tournament.tournamentStaff.some(staff => staff.playerId === currentPlayer.id && staff.role === TournamentStaffRole.Organiser)) {
+                    this.tournament = tournament;
+                } else {
+                    this.accessDenied = true;
+                }
+                this.isLoading = false;
+            });
         });
     }
 

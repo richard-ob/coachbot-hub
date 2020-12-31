36948398d18690a2da504dfd2e35639125c49ed7
Richard O'Brien <richard.obrien4+git@gmail.com>
2020-12-31 13:27:20 +0000

    Remove commented out code

--- CoachBot.API/Program.cs
+++ CoachBot.API/Program.cs
@@ -46,31 +46,5 @@ namespace CoachBot
 
             await Task.Delay(Timeout.Infinite);
         }
-
-            /*
-            private static void Main(string[] args) =>
-                new Program().RunAsync().GetAwaiter().GetResult();
-
-            private async Task RunAsync()
-            {
-                var config = ConfigHelper.GetConfig();
-                var httpPort = config.WebServerConfig.ApiPort > 0 ? config.WebServerConfig.ApiPort : 8080;
-                var httpsPort = config.WebServerConfig.SecureApiPort > 0 ? config.WebServerConfig.SecureApiPort : 44380;
-                var host = WebHost
-                  .CreateDefaultBuilder()
-                  .UseKestrel(options =>
-                  {
-                      options.Listen(IPAddress.Any, httpPort);
-                      options.Listen(IPAddress.Any, httpsPort, listenOptions =>
-                      {
-                         // listenOptions.UseHttps(config.WebServerConfig.SecurityCertFile, config.WebServerConfig.SecurityCertPassword);
-                      });
-                  })
-                  .UseStartup<WebStartup>()               
-                  .Build();
-                host.Start();
-
-                await Task.Delay(Timeout.Infinite);
-            }*/
-        }
+    }
 }
